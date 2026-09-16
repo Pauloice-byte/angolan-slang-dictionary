f79a5b48fe96403d7597454f2ec8d5f77424141e
Pauloice-byte <pauloice36@gmail.com>
2026-09-16 13:17:11 +0100

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://zuxgwhvnmydxkehbstqd.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_A4Ubsp2eDN8XSclvzkfw5A_HZZMrjKh";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
